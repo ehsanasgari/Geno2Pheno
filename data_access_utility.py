5421822a1d2931c387ea03ff1387ecd126d6091e
ehsanasgari <asgari@berkeley.edu>
2017-09-21 15:30:58 +0200

    rename creation files chi2

--- data_access_utility.py
+++ data_access_utility.py
@@ -5,137 +5,34 @@ __version__ = "1.0.0"
 __maintainer__ = "Ehsaneddin Asgari"
 __email__ = "asgari@berkeley.edu"
 
-from file_utility import  FileUtility
+from file_utility import FileUtility
 from scipy import sparse
 from sklearn.feature_extraction.text import TfidfTransformer
-import codecs
 import numpy as np
-import itertools
+from data_create_utility import ABRDataCreate
 
-class ABRAccessUtility(object):
+
+class ABRDataAccess(object):
     '''
-    This class is written to read/load/save
-    data for AMR prediction of Pseudomonas Aeruginosa
+    This class is written to load data for ABR prediction/analysis of Pseudomonas Aeruginosa
     '''
+
     def __init__(self, dir, prefix_list):
+        '''
+        :param dir: directory of features
+        :param prefix_list: list of prefixes for features
+        '''
         print ('Data access created..')
+        # dictionary of feature types
+        self.X = dict()
+        # dictionary of feature names
+        self.feature_names = dict()
+        # dictionary of isolates
+        self.isolates = dict()
+        ## load the features
         self.load_data(dir, prefix_list)
-        self.load_labels()
-
-    def getXYPredictionMat_multidrug(self):
-        # get common isolates
-        list_of_list_of_isolates=list(self.isolates.values())
-        mapping=self.get_multilabel_labels()
-        list_of_list_of_isolates.append(list(mapping.keys()))
-        final_isolates=ABRAccessUtility.common_isolates(list_of_list_of_isolates)
-        final_isolates.sort()
-
-        # feature types
-        features=list(self.X.keys())
-
-        tf=TfidfTransformer(norm=None, use_idf=True, smooth_idf=True)
-
-        feature_names=[]
-        feature_matrices=[]
-        for feature in features:
-            if not feature=='geneexp':
-                tf.fit(self.X[feature])
-                temp=tf.transform(self.X[feature])
-            else:
-                temp=self.X[feature]
-            idx=[self.isolates[feature].index(isol) for isol in final_isolates]
-            temp=temp[idx,:]
-            feature_matrices.append(temp.toarray())
-            feature_names+=['##'.join([feature, x]) for x in self.feature_names[feature]]
-
-        X=np.concatenate(tuple(feature_matrices), axis=1)
-        X=sparse.csr_matrix(X)
-        Y=[mapping[isol] for isol in final_isolates]
-        return X, Y, feature_names
-    
-    def getXYPredictionMat(self, drug, mapping={'0':0,'0.0':0,'1':1,'1.0':1}):
-        # get common isolates
-        list_of_list_of_isolates=list(self.isolates.values())
-        mapping=dict(self.get_labels(mapping)[drug])
-        list_of_list_of_isolates.append(list(mapping.keys()))
-        final_isolates=ABRAccessUtility.common_isolates(list_of_list_of_isolates)
-        final_isolates.sort()
-
-        # feature types
-        features=list(self.X.keys())
-
-        tf=TfidfTransformer(norm=None, use_idf=True, smooth_idf=True)
-
-        feature_names=[]
-        feature_matrices=[]
-        for feature in features:
-            if not feature=='geneexp':
-                tf.fit(self.X[feature])
-                temp=tf.transform(self.X[feature])
-            else:
-                temp=self.X[feature]
-            idx=[self.isolates[feature].index(isol) for isol in final_isolates]
-            temp=temp[idx,:]
-            feature_matrices.append(temp.toarray())
-            feature_names+=['##'.join([feature, x]) for x in self.feature_names[feature]]
-
-        X=np.concatenate(tuple(feature_matrices), axis=1)
-        X=sparse.csr_matrix(X)
-        Y=[mapping[isol] for isol in final_isolates]
-        return X, Y, feature_names
-
-    def getXYPredictionMat_scaffolds(self, drug, mapping={'0':0,'0.0':0,'1':1,'1.0':1}):
-        # get common isolates
-        list_of_list_of_isolates=list(self.isolates.values())
-        mapping=dict(self.get_labels(mapping)[drug])
-        list_of_list_of_isolates.append(list(mapping.keys()))
-        final_isolates=ABRAccessUtility.common_isolates(list_of_list_of_isolates)
-        final_isolates.sort()
-
-        # feature types
-        features=list(self.X.keys())
-
-        tf=TfidfTransformer(norm='l2', use_idf=True, smooth_idf=True)
-
-        feature_names=[]
-        feature_matrices=[]
-        for feature in features:
-            temp=self.X[feature]
-            idx=list(itertools.chain(*[[ids for ids, x in enumerate(self.isolates[feature]) if x==isol]  for isol in final_isolates]))
-            temp=temp[idx,:]
-            feature_matrices.append(temp.toarray())
-            feature_names+=['##'.join([feature, x]) for x in self.feature_names[feature]]
-
-        X=np.concatenate(tuple(feature_matrices), axis=1)
-        X=sparse.csr_matrix(X)
-        Y=[mapping[self.isolates[features[0]][ids]] for ids in idx]
-        return X, Y, feature_names
-
-    def get_labels(self,mapping={'0':0,'0.0':0,'1':1,'1.0':1}):
-        '''
-        Load labels
-        :param mapping:
-        :return:
-        '''
-        new_drug2labeledisolates_mapping=dict()
-        for drug in self.drugs:
-            new_drug2labeledisolates_mapping[drug]=[]
-
-        # only consider non-empty values
-        for isolate,resist_vec in self.isolate2label_vec_mapping.items():
-            for idx, val in enumerate(resist_vec):
-                if val in mapping:
-                    new_drug2labeledisolates_mapping[self.drugs[idx]].append((isolate,mapping[val]))
-        return new_drug2labeledisolates_mapping
-
-    def get_multilabel_labels(self):
-        '''
-            drug resistance profile label
-        '''
-        mapping={'':'I','0':'V','0.0':'V','1':'R','1.0':'R'}
-        return {k:''.join([mapping[x] for x in list(v)]) for k,v in self.isolate2label_vec_mapping.items()}
-
-
+        ## load the labels
+        self.BasicDataObj = ABRDataCreate()
 
     def load_data(self, dir, prefix_list):
         '''
@@ -144,37 +41,90 @@ class ABRAccessUtility(object):
         :param prefix_list:
         :return:
         '''
-        self.X=dict()
-        self.feature_names=dict()
-        self.isolates=dict()
         for save_pref in prefix_list:
-            self.X[save_pref]=FileUtility.load_sparse_csr('_'.join([dir+save_pref,'feature','vect.npz']))
-            self.feature_names[save_pref]=FileUtility.load_list('_'.join([dir+save_pref,'feature','list.txt']))
-            self.isolates[save_pref]=FileUtility.load_list('_'.join([dir+save_pref,'isolates','list.txt']))
+            self.X[save_pref] = FileUtility.load_sparse_csr('_'.join([dir + save_pref, 'feature', 'vect.npz']))
+            self.feature_names[save_pref] = FileUtility.load_list('_'.join([dir + save_pref, 'feature', 'list.txt']))
+            self.isolates[save_pref] = FileUtility.load_list('_'.join([dir + save_pref, 'isolates', 'list.txt']))
 
-    def load_labels(self):
+    def get_xy_prediction_mats(self, drug, mapping={'0': 0, '0.0': 0, '1': 1, '1.0': 1}, features_for_idf=[]):
         '''
-            This function load labels ZG02420619 ['1.0', '0.0', '0.0', '', '1.0']
+        :param drug:
+        :param mapping:
+        :param features_for_idf: if needed..
+        :return:
         '''
-        label_file_address='/mounts/data/proj/asgari/github_data/data/pseudomonas/data_v2/mic_bin_without_intermediate.txt'
-        rows=[l.replace('\n','') for l in codecs.open(label_file_address,'r','utf-8').readlines()]
-        self.isolate2label_vec_mapping={str(entry.split('\t')[0]):[str(x) for idx,x in enumerate(entry.split('\t')[1::])] for entry in rows[1::]}
-        self.drugs=rows[0].rstrip().split('\t')[1::]
+        ## find a mapping from isolates to the labels
+        mapping_isolate_label = dict(self.BasicDataObj.get_new_labeling(mapping)[drug])
 
-        # init
-        self.drug2labeledisolates_mapping=dict()
-        for drug in self.drugs:
-            self.drug2labeledisolates_mapping[drug]=[]
+        # get common isolates
+        list_of_list_of_isolates = list(self.isolates.values())
+        list_of_list_of_isolates.append(list(mapping_isolate_label.keys()))
+        final_isolates = ABRDataAccess.common_isolates(list_of_list_of_isolates)
+        final_isolates.sort()
 
-        mapping={'0':0,'0.0':0,'1':1,'1.0':1}
-        # only consider non-empty values
-        for isolate,resist_vec in self.isolate2label_vec_mapping.items():
-            for idx, val in enumerate(resist_vec):
-                if val in mapping:
-                    self.drug2labeledisolates_mapping[self.drugs[idx]].append((isolate,mapping[val]))
-        # generate dict of labels for each class
-        for drug in self.drugs:
-            self.drug2labeledisolates_mapping[drug]=dict(self.drug2labeledisolates_mapping[drug])
+        # feature types
+        feature_types = list(self.X.keys())
+
+        # to apply idf if necessary
+        if len(features_for_idf) > 0:
+            tf = TfidfTransformer(norm=None, use_idf=True, smooth_idf=True)
+
+        feature_names = []
+        feature_matrices = []
+        for feature_type in feature_types:
+            if feature_type in features_for_idf:
+                tf.fit(self.X[feature_type])
+                temp = tf.transform(self.X[feature_type])
+            else:
+                temp = self.X[feature_type]
+            idx = [self.isolates[feature_type].index(isol) for isol in final_isolates]
+            temp = temp[idx, :]
+            feature_matrices.append(temp.toarray())
+            feature_names += ['##'.join([feature_type, x]) for x in self.feature_names[feature_type]]
+
+        X = np.concatenate(tuple(feature_matrices), axis=1)
+        X = sparse.csr_matrix(X)
+        Y = [mapping[isol] for isol in final_isolates]
+        return X, Y, feature_names
+
+    def get_xy_multidrug_prediction_mats(self, features_for_idf=[]):
+        '''
+        :param features_for_idf: if needed..
+        :return:
+        '''
+        ## find a mapping from isolates to the labels
+        mapping_isolate_label = dict(self.BasicDataObj.get_multilabel_label_dic())
+
+        # get common isolates
+        list_of_list_of_isolates = list(self.isolates.values())
+        list_of_list_of_isolates.append(list(mapping_isolate_label.keys()))
+        final_isolates = ABRDataAccess.common_isolates(list_of_list_of_isolates)
+        final_isolates.sort()
+
+        # feature types
+        feature_types = list(self.X.keys())
+
+        # to apply idf if necessary
+        if len(features_for_idf) > 0:
+            tf = TfidfTransformer(norm=None, use_idf=True, smooth_idf=True)
+
+        feature_names = []
+        feature_matrices = []
+        for feature_type in feature_types:
+            if feature_type in features_for_idf:
+                tf.fit(self.X[feature_type])
+                temp = tf.transform(self.X[feature_type])
+            else:
+                temp = self.X[feature_type]
+            idx = [self.isolates[feature_type].index(isol) for isol in final_isolates]
+            temp = temp[idx, :]
+            feature_matrices.append(temp.toarray())
+            feature_names += ['##'.join([feature_type, x]) for x in self.feature_names[feature_type]]
+
+        X = np.concatenate(tuple(feature_matrices), axis=1)
+        X = sparse.csr_matrix(X)
+        Y = [mapping_isolate_label[isol] for isol in final_isolates]
+        return X, Y, feature_names
 
     @staticmethod
     def common_isolates(list_of_list_of_isolates):
@@ -182,44 +132,9 @@ class ABRAccessUtility(object):
         :param list_of_list_of_isolates:
         :return:
         '''
-        common_islt=set(list_of_list_of_isolates[0])
+        common_islt = set(list_of_list_of_isolates[0])
         for next_list in list_of_list_of_isolates[1::]:
-            common_islt=common_islt.intersection(next_list)
-        common_islt=list(common_islt)
+            common_islt = common_islt.intersection(next_list)
+        common_islt = list(common_islt)
         common_islt.sort()
-        return common_islt 
-    
-    @staticmethod
-    def read_data_from_scratch():
-        '''
-        Used only once to create the data for later loading
-        :return:
-        '''
-        data_dir='/mounts/data/proj/asgari/github_data/data/pseudomonas/data_v2/'
-        genexp_data_addr='rpg_log_transformed_426.txt'
-        snps_data_addr='all_SNPs_final_bin_uq.txt'
-        gen_pres_abs_data_addr='annot.txt'
-        FileUtility._read_signle_file_continuous_feature(data_dir+genexp_data_addr,save_pref=data_dir+'geneexp')
-        FileUtility._read_signle_file_binary_feature(data_dir+snps_data_addr,save_pref=data_dir+'SNPs')
-        FileUtility._read_signle_file_binary_feature(data_dir+gen_pres_abs_data_addr,save_pref=data_dir+'genePA')
-
-    def read_assembled_sequences(self):
-        '''
-        Used only once to create the data for later loading
-        :return:
-        '''
-        data_dir='/mounts/data/proj/asgari/github_data/data/pseudomonas/data_v2/sequences_assembled/'
-        save_dir='/mounts/data/proj/asgari/github_data/data/pseudomonas/data_v2/extracted_features/'
-        fasta_files=FileUtility.recursive_glob(data_dir,'*.fasta')
-        FileUtility._read_fasta_kmer_features(fasta_files,save_pref=save_dir+'k4-9mers',analyzer='char', ngram=(4,9), idf=True, aggregate=True)
-        
-    def create_labeled_scaffolds(self,min_length=10000):
-        '''
-        To create sequence scaffold for prediction
-        '''
-        joint_labels=self.get_multilabel_labels()
-        data_dir='/mounts/data/proj/asgari/github_data/data/pseudomonas/data_v2/sequences_assembled/'
-        save_dir='/mounts/data/proj/asgari/github_data/data/pseudomonas/data_v2/extracted_features/'
-        fasta_files=FileUtility.recursive_glob(data_dir,'*.fasta')
-        FileUtility._create_fasta_sequences(fasta_files, save_dir+'labeled_sequences_min_'+str(min_length)+'.fasta', joint_labels, min_length=min_length)
-
+        return common_islt
--- file_utility.py
+++ file_utility.py
@@ -8,100 +8,13 @@ __email__ = "asgari@berkeley.edu"
 import codecs
 import numpy as np
 from scipy import sparse
-from sklearn.feature_extraction.text import TfidfVectorizer
 import fnmatch
 import os
-from Bio import SeqIO
-from featurizer import TextFeature
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from Bio.Alphabet import generic_dna
 import _pickle as pickle
 
 class FileUtility(object):
     def __init__(self):
         print ('File utility object created..')
-
-
-    @staticmethod
-    def _read_fasta_kmer_features(fasta_files, save_pref='_', analyzer='char', ngram=(4,8), idf=True, aggregate=True):
-        corpus=[]
-        isolates=[]
-        for fasta_file in fasta_files:
-            iso=fasta_file.split('/')[-1].split('_')[0]
-            line=''
-            isolates.append(iso)
-            if aggregate:
-                for cur_record in SeqIO.parse(fasta_file, "fasta") :
-                    line+=str(cur_record.seq).upper().replace(' ','')
-                corpus.append(line)
-            else:
-                for cur_record in SeqIO.parse(fasta_file, "fasta") :
-                    temp=str(cur_record.seq).upper().replace(' ','')
-                    if len(temp)>len(line):
-                        line=temp
-                corpus.append(line)
-
-        TF=TextFeature(corpus, analyzer=analyzer, ngram=ngram, idf=idf, norm='l2')
-
-        FileUtility.save_sparse_csr('_'.join([save_pref,'feature','vect.npz']),TF.tf_vec)
-        FileUtility.save_list('_'.join([save_pref,'feature','list.txt']), TF.feature_names)
-        FileUtility.save_list('_'.join([save_pref,'isolates','list.txt']),isolates)
-
-    @staticmethod
-    def _create_fasta_sequences(fasta_files, filename, mapping, min_length=100000):
-        records=[]
-        for fasta_file in fasta_files:
-            iso=fasta_file.split('/')[-1].split('_')[0]
-            if iso in mapping:
-                selected_sequence=''
-                isolate='###'.join([iso,mapping[iso]])
-
-                for cur_record in SeqIO.parse(fasta_file, "fasta") :
-                    seq_select=str(cur_record.seq).upper()
-                    if len(seq_select)>min_length:
-                        records.append(SeqRecord(Seq(seq_select, generic_dna), id=isolate, description=''))
-        print (len(records), ' records added for training')
-        SeqIO.write(records, filename, "fasta")
-        
-
-    @staticmethod
-    def _read_signle_file_binary_feature(path, save_pref='_' ):
-        '''
-        :param path:
-        :return:
-        '''
-        rows=[l.strip() for l in codecs.open(path,'r','utf-8').readlines()]
-        features=[feat.replace(' ','') for feat in rows[0].rstrip().split('\t')]
-        isolates=[row.split()[0] for row in rows[1::]]
-
-        # corpus creation
-        corpus=[]
-        for entry in rows[1::]:
-            corpus.append(' '.join([features[idx] for idx,x in enumerate(entry.split('\t')[1::]) if FileUtility._float_or_zero(x)>0]))
-
-        tfm = TfidfVectorizer(use_idf=False, analyzer='word', norm=None, tokenizer=str.split, stop_words=[], lowercase=False)
-        tf_vec = tfm.fit_transform(corpus)
-        feature_names = tfm.get_feature_names()
-
-        FileUtility.save_sparse_csr('_'.join([save_pref,'feature','vect.npz']),tf_vec)
-        FileUtility.save_list('_'.join([save_pref,'feature','list.txt']),feature_names)
-        FileUtility.save_list('_'.join([save_pref,'isolates','list.txt']),isolates)
-
-    @staticmethod
-    def _read_signle_file_continuous_feature(path, save_pref='_' ):
-        '''
-        :param path:
-        :return:
-        '''
-        rows=[l.strip() for l in codecs.open(path,'r','utf-8').readlines()]
-        tf_vec=sparse.csr_matrix([[FileUtility._float_or_zero(x)  for x in entry.split('\t')[1::]] for entry in rows[1::]])
-        isolates=[row.split()[0] for row in rows[1::]]
-        feature_names=[feat.replace(' ','') for feat in rows[0].rstrip().split('\t')]
-
-        FileUtility.save_sparse_csr('_'.join([save_pref,'feature','vect.npz']),tf_vec)
-        FileUtility.save_list('_'.join([save_pref,'feature','list.txt']),feature_names)
-        FileUtility.save_list('_'.join([save_pref,'isolates','list.txt']),isolates)
 
     @staticmethod
     def save_obj(value, filename):
@@ -132,7 +45,6 @@ class FileUtility(object):
         loader = np.load(filename)
         return sparse.csr_matrix((  loader['data'], loader['indices'], loader['indptr']),
                              shape = loader['shape'])
-
     @staticmethod
     def _float_or_zero(value):
         try:
