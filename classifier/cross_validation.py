f71b833e471adb88d2ea1751b1b94012c8ba3529
ehsanasgari <asgari@berkeley.edu>
2018-02-26 16:33:12 +0100

    add group k-fold

--- classifier/cross_validation.py
+++ classifier/cross_validation.py
@@ -162,3 +162,39 @@ class NestedCrossVal(CrossValidator):
         FileUtility.save_obj([self.greed_search,self.nested_score],file_name)
 
 
+class GroupKFoldCrossVal(CrossValidator):
+    '''
+        K-fold cross-validation tuning and evaluation
+    '''
+    def __init__(self, X, Y, folds=10, random_state=1, isolate_list, fold_file):
+        '''
+        :param X:
+        :param Y:
+        :param folds:
+        :param random_state:
+        '''
+        CrossValidator.__init__(self, X, Y)
+        self.cv = GroupKFold(n_splits=folds, shuffle=True, random_state=random_state)
+        self.X = X
+        self.Y = Y
+
+    def tune_and_evaluate(self, estimator, parameters, score='f1_macro', n_jobs=-1, file_name='results'):
+        '''
+        :param estimator:
+        :param parameters:p
+        :param score:
+        :param n_jobs:
+        :param file_name: directory/tuning/classifier/features/
+        :return:
+        '''
+        # greed_search
+        self.greed_search = GridSearchCV(estimator=estimator, param_grid=parameters, cv=self.cv, scoring=self.scoring,
+                                         refit=score, error_score=0, n_jobs=n_jobs)
+
+        label_set=list(set(self.Y))
+        # fitting
+        self.greed_search.fit(X=self.X, y=self.Y)
+        y_predicted = cross_val_predict(self.greed_search.best_estimator_, self.X, self.Y)
+        conf=confusion_matrix(self.Y,y_predicted,labels=label_set)
+        # save in file
+        FileUtility.save_obj(file_name, [label_set, conf, self.greed_search.best_score_, self.greed_search.best_estimator_, self.greed_search.cv_results_, self.greed_search.best_params_, (y_predicted,label_set)])
