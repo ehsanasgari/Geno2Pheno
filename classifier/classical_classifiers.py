f6d530f6df143bbbd00ae2efdd7297d4816c2066
ehsanasgari <asgari@berkeley.edu>
2018-05-28 14:35:53 +0200

    classifiers cleanup

--- classifier/classical_classifiers.py
+++ classifier/classical_classifiers.py
@@ -39,8 +39,16 @@ class SVM:
     def tune_and_eval(self, results_file,
                       params=[{'C': [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1, 0.2, 0.5, 0.01, 0.02, 0.05, 0.001],
                                'penalty': ['l1'], "tol": [1e-06, 1e-04], 'dual': [False, True], "fit_intercept": [True],
-                               'loss': ['l2'], 'class_weight': ['balanced', None]}], njobs=50):
-        CV = KFoldCrossVal(self.X, self.Y, folds=10)
+                               'loss': ['l2'], 'class_weight': ['balanced', None]}], njobs=50, kfold=10):
+        '''
+        K-fold cross-validation
+        :param results_file: file to save the results
+        :param params: parameters to be tuned
+        :param njobs: number of cores
+        :param kfold: number of folds
+        :return:
+        '''
+        CV = KFoldCrossVal(self.X, self.Y, folds=kfold)
         CV.tune_and_evaluate(self.model, parameters=params, score='f1_macro', file_name=results_file + '_SVM',
                              n_jobs=njobs)
 
@@ -49,10 +57,11 @@ class SVM:
          "tol": [1e-06, 1e-04], 'dual': [False, True], "fit_intercept": [True], 'loss': ['l2'],
          'class_weight': ['balanced', None]}], njobs=50):
         '''
-        Tune, evaluate, extract features (if a list of features are provided)
         :param results_file:
+        :param isolates:
+        :param folds:
         :param params:
-        :param feature_names:
+        :param njobs:
         :return:
         '''
         self.CV = PredefinedFoldCrossVal(self.X, self.Y, isolates, folds)
@@ -66,6 +75,11 @@ class LogRegression:
     '''
 
     def __init__(self, X, Y):
+        '''
+
+        :param X:
+        :param Y:
+        '''
         self.model = LogisticRegression(C=1.0)
         self.X = X
         self.Y = Y
@@ -73,8 +87,15 @@ class LogRegression:
     def tune_and_eval(self, results_file,
                       params=[{'C': [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1, 0.2, 0.5, 0.01, 0.02, 0.05, 0.001],
                                'penalty': ['l1'], "tol": [1e-06, 1e-04], 'dual': [False, True], "fit_intercept": [True],
-                               'class_weight': ['balanced', None], 'solver': ['liblinear']}], njobs=50):
-        CV = KFoldCrossVal(self.X, self.Y, folds=10)
+                               'class_weight': ['balanced', None], 'solver': ['liblinear']}], njobs=50, kfold=10):
+        '''
+        :param results_file:
+        :param params:
+        :param njobs:
+        :param kfold:
+        :return:
+        '''
+        CV = KFoldCrossVal(self.X, self.Y, folds=kfold)
         CV.tune_and_evaluate(self.model, parameters=params, score='f1_macro', file_name=results_file + '_LR',
                              n_jobs=njobs)
 
@@ -83,10 +104,11 @@ class LogRegression:
          "tol": [1e-06, 1e-04], 'dual': [False, True], "fit_intercept": [True], 'class_weight': ['balanced', None],
          'solver': ['liblinear']}], njobs=50):
         '''
-        Tune, evaluate, extract features (if a list of features are provided)
         :param results_file:
+        :param isolates:
+        :param folds:
         :param params:
-        :param feature_names:
+        :param njobs:
         :return:
         '''
         self.CV = PredefinedFoldCrossVal(self.X, self.Y, isolates, folds)
@@ -100,18 +122,23 @@ class RFClassifier:
     '''
 
     def __init__(self, X, Y):
+        '''
+        :param X:
+        :param Y:
+        '''
         self.model = RandomForestClassifier(bootstrap=True, criterion='gini',
                                             min_samples_split=2, max_features='auto', min_samples_leaf=1,
                                             n_estimators=1000)
         self.X = X
         self.Y = Y
 
-    def tune_and_eval(self, results_file, params=None, feature_names=None, njobs=50):
+    def tune_and_eval(self, results_file, params=None, feature_names=None, njobs=50, kfold=10):
         '''
-        Tune, evaluate, extract features (if a list of features are provided)
         :param results_file:
         :param params:
         :param feature_names:
+        :param njobs:
+        :param kfold:
         :return:
         '''
         if params is None:
@@ -120,7 +147,7 @@ class RFClassifier:
                        'max_features': ['sqrt', 'auto'],  # 'auto',
                        'min_samples_split': [2, 5, 10],  # 2,5,10
                        'min_samples_leaf': [1], 'class_weight': ['balanced', None]}]
-        self.CV = KFoldCrossVal(self.X, self.Y, folds=10)
+        self.CV = KFoldCrossVal(self.X, self.Y, folds=kfold)
         self.CV.tune_and_evaluate(self.model, parameters=params, score='f1_macro', file_name=results_file + '_RF',
                                   n_jobs=njobs)
         if feature_names is not None:
@@ -130,10 +157,12 @@ class RFClassifier:
 
     def tune_and_eval_predefined(self, results_file, isolates, folds, params=None, feature_names=None, njobs=50):
         '''
-        Tune, evaluate, extract features (if a list of features are provided)
         :param results_file:
+        :param isolates:
+        :param folds:
         :param params:
         :param feature_names:
+        :param njobs:
         :return:
         '''
         if params is None:
@@ -151,6 +180,13 @@ class RFClassifier:
             self.generate_RF_important_features(best_estimator_, feature_names, results_file, 1000)
 
     def generate_RF_important_features(self, clf_random_forest, feature_names, results_file, N):
+        '''
+        :param clf_random_forest:
+        :param feature_names:
+        :param results_file:
+        :param N:
+        :return:
+        '''
         file_name = results_file + 'RF_features'
         clf_random_forest.fit(self.X, self.Y)
         std = np.std([tree.feature_importances_ for tree in clf_random_forest.estimators_], axis=0)
@@ -175,23 +211,35 @@ class KNN:
     '''
 
     def __init__(self, X, Y):
+        '''
+        :param X:
+        :param Y:
+        '''
         self.model = KNeighborsClassifier(n_neighbors=3)
         self.X = X
         self.Y = Y
 
-    def tune_and_eval(self, results_file, params=None, njobs=50):
+    def tune_and_eval(self, results_file, params=None, njobs=50, kfold=10):
+        '''
+        :param results_file:
+        :param params:
+        :param njobs:
+        :param kfold:
+        :return:
+        '''
         if params is None:
             params = [{"n_neighbors": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20], 'weights': ['uniform', 'distance']}]
-        self.CV = KFoldCrossVal(self.X, self.Y, folds=10)
+        self.CV = KFoldCrossVal(self.X, self.Y, folds=kfold)
         self.CV.tune_and_evaluate(self.model, parameters=params, score='f1_macro', file_name=results_file + '_KNN',
                                   n_jobs=njobs)
 
     def tune_and_eval_predefined(self, results_file, isolates, folds, params=None, njobs=50):
         '''
-        Tune, evaluate, extract features (if a list of features are provided)
         :param results_file:
+        :param isolates:
+        :param folds:
         :param params:
-        :param feature_names:
+        :param njobs:
         :return:
         '''
         if params is None:
