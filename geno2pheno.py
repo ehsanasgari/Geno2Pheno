247b9ddc5a8b511c226c01ebcc04c52f91ae1a8a
ehsanasgari <asgari@berkeley.edu>
2018-06-06 14:51:56 +0200

    xml interface

--- geno2pheno.py
+++ geno2pheno.py
@@ -12,12 +12,13 @@ __website__ = ""
 import argparse
 import os
 import os.path
+import shutil
 import sys
+from xml.dom import minidom
 
 from data_access.intermediate_representation_utility import IntermediateRepCreate
 from utility.file_utility import FileUtility
-from xml.dom import minidom
-import shutil
+
 
 class Geno2Pheno:
     def __init__(self, genml_path, override=False):
@@ -30,55 +31,61 @@ class Geno2Pheno:
 
         # parse project part
         project = xmldoc.getElementsByTagName('project')
-        output=project[0].attributes['output'].value
-        project_name=project[0].attributes['name'].value
+        output = project[0].attributes['output'].value
+        project_name = project[0].attributes['name'].value
 
         if override and os.path.exists(output):
             var = input("Delete existing files at the output path? (y/n)")
-            if var =='y':
+            if var == 'y':
                 shutil.rmtree(output)
         if not os.path.exists(output):
             os.makedirs(output)
 
-        log_file=output+'/'+'logfile'
-        log_info=['Project '+project_name]
+        log_file = output + '/' + 'logfile'
+        log_info = ['Project ' + project_name]
         log_info.append('')
 
-        representation_path=output+'/intermediate_rep/'
-        IC=IntermediateRepCreate(representation_path)
+        representation_path = output + '/intermediate_rep/'
+        IC = IntermediateRepCreate(representation_path)
 
         # load tables
         tabless = xmldoc.getElementsByTagName('tables')
         for tables in tabless:
-            path=tables.attributes['path'].value
-            normalization=tables.attributes['normalization'].value
-            prefix=tables.firstChild.nodeValue.strip()
-            for file in FileUtility.recursive_glob(path,'*'):
-                IC.create_table(file,prefix+file.split('/')[-1],normalization, override)
-                log_info.append(prefix+file.split('/')[-1], ' created, where override is ',override)
+            path = tables.attributes['path'].value
+            normalization = tables.attributes['normalization'].value
+            prefix = tables.firstChild.nodeValue.strip() + '_'
+            if len(prefix) == 1:
+                prefix = ''
+            for file in FileUtility.recursive_glob(path, '*'):
+                IC.create_table(file, prefix + file.split('/')[-1], normalization, override)
+                log_info.append(prefix + file.split('/')[-1] + ' created, where override is ', str(override))
 
         tables = xmldoc.getElementsByTagName('table')
         for table in tables:
-            path=table.attributes['path'].value
-            normalization=table.attributes['normalization'].value
-            prefix=tables.firstChild.nodeValue.strip()
-            IC.create_table(path,prefix+path.split('/')[-1],normalization, override)
-            log_info.append(prefix+path.split('/')[-1], ' created, where override is ',override)
+            path = table.attributes['path'].value
+            normalization = table.attributes['normalization'].value
+            prefix = tables.firstChild.nodeValue.strip() + '_'
+            if len(prefix) == 1:
+                prefix = ''
+            IC.create_table(path, prefix + path.split('/')[-1], normalization, override)
+            log_info.append(prefix + path.split('/')[-1] + ' created, where override is ' + str(override))
 
         ## Adding metadata
-        metadata_path=output+'/metadata/'
+        metadata_path = output + '/metadata/'
         if not os.path.exists(metadata_path):
             os.makedirs(metadata_path)
         # phenotype
         phenotype = xmldoc.getElementsByTagName('phenotype')
         if not os.path.exists(metadata_path + 'phenotypes.txt') or override:
-            FileUtility.save_list(metadata_path + 'phenotypes.txt', FileUtility.load_list(phenotype[0].attributes['path'].value))
+            FileUtility.save_list(metadata_path + 'phenotypes.txt',
+                                  FileUtility.load_list(phenotype[0].attributes['path'].value))
 
         # tree
         phylogentictree = xmldoc.getElementsByTagName('phylogentictree')
         if not os.path.exists(metadata_path + 'phylogentictree.txt') or override:
-            FileUtility.save_list(metadata_path +'phylogentictree.txt', FileUtility.load_list(phylogentictree[0].attributes['path'].value))
-
+            FileUtility.save_list(metadata_path + 'phylogentictree.txt',
+                                  FileUtility.load_list(phylogentictree[0].attributes['path'].value))
+        FileUtility.save_list(log_file, log_info)
 
 
 def checkArgs(args):
@@ -104,7 +111,7 @@ def checkArgs(args):
         err = err + "\nError: Permission denied or could not find the labels!"
         return err
 
-    G2P=Geno2Pheno(parsedArgs.genml_path, parsedArgs.override)
+    G2P = Geno2Pheno(parsedArgs.genml_path, parsedArgs.override)
 
     return False
 
