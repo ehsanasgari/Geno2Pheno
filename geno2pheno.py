71d73b8dd6a4e9e5d51d1cdabe515b69a29a6720
ehsanasgari <asgari@berkeley.edu>
2018-06-07 17:37:01 +0200

    dataaccess

--- geno2pheno.py
+++ geno2pheno.py
@@ -95,6 +95,83 @@ class Geno2Pheno:
         FileUtility.save_list(log_file, log_info)
 
 
+class Classify:
+    def __init__(self, genml_path, override=1, cores=4):
+        '''
+            Geno2Pheno commandline use
+        '''
+        print('Geno2Pheno of Seq2Geno2Pheno 1.0.0')
+
+        xmldoc = minidom.parse(genml_path)
+
+        # parse project part
+        project = xmldoc.getElementsByTagName('project')
+        output = project[0].attributes['output'].value
+        project_name = project[0].attributes['name'].value
+
+        if override and os.path.exists(output):
+            var = input("Delete existing files at the output path? (y/n)")
+            if var == 'y':
+                shutil.rmtree(output)
+        if not os.path.exists(output):
+            os.makedirs(output)
+
+        log_file = output + '/' + 'logfile'
+        log_info = ['Project ' + project_name]
+
+
+        representation_path = output + '/intermediate_rep/'
+        IC = IntermediateRepCreate(representation_path)
+
+        # load tables
+        tabless = xmldoc.getElementsByTagName('tables')
+        for tables in tabless:
+            path = tables.attributes['path'].value
+            normalization = tables.attributes['normalization'].value
+            prefix = tables.firstChild.nodeValue.strip() + '_'
+            if len(prefix) == 1:
+                prefix = ''
+            for file in FileUtility.recursive_glob(path, '*'):
+                log=IC.create_table(file, prefix + file.split('/')[-1], normalization, override)
+                log_info.append(log)
+
+        tables = xmldoc.getElementsByTagName('table')
+        for table in tables:
+            path = table.attributes['path'].value
+            normalization = table.attributes['normalization'].value
+            prefix = tables.firstChild.nodeValue.strip() + '_'
+            if len(prefix) == 1:
+                prefix = ''
+            log=IC.create_table(path, prefix + path.split('/')[-1], normalization, override)
+            log_info.append(log)
+
+        # load sequences
+        sequences = xmldoc.getElementsByTagName('sequence')
+        for sequence in sequences:
+            path = sequence.attributes['path'].value
+            kmer = int(sequence.attributes['kmer'].value)
+            log=IC.create_kmer_table(path,kmer,cores=cores)
+            log_info.append(log)
+
+        ## Adding metadata
+        metadata_path = output + '/metadata/'
+        if not os.path.exists(metadata_path):
+            os.makedirs(metadata_path)
+        # phenotype
+        phenotype = xmldoc.getElementsByTagName('phenotype')
+        if not os.path.exists(metadata_path + 'phenotypes.txt') or override:
+            FileUtility.save_list(metadata_path + 'phenotypes.txt',
+                                  FileUtility.load_list(phenotype[0].attributes['path'].value))
+
+        # tree
+        phylogentictree = xmldoc.getElementsByTagName('phylogentictree')
+        if not os.path.exists(metadata_path + 'phylogentictree.txt') or override:
+            FileUtility.save_list(metadata_path + 'phylogentictree.txt',
+                                  FileUtility.load_list(phylogentictree[0].attributes['path'].value))
+        FileUtility.save_list(log_file, log_info)
+
+
+
 def checkArgs(args):
     '''
         This function checks the input arguments and returns the errors (if exist) otherwise reads the parameters
@@ -116,6 +193,10 @@ def checkArgs(args):
                         help='Number of cores to be used')
 
     # svm ######################################################################################################
+    parser.add_argument('--svm', action='store', dest='svm', default=False, type=str,
+                        help='perform SVM')
+
+
 
     parsedArgs = parser.parse_args()
 
@@ -126,6 +207,8 @@ def checkArgs(args):
     if parsedArgs.genml_path:
         G2P = Geno2Pheno(parsedArgs.genml_path, parsedArgs.override, parsedArgs.cores)
 
+    elif parsedArgs.svm:
+        G2P = Geno2Pheno(parsedArgs.svm, parsedArgs.override, parsedArgs.cores)
 
     return False
 
