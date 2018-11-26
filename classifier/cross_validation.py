9ada1f1b82062bc6fad68f438cf261a527457b64
ehsanasgari <asgari@berkeley.edu>
2018-11-26 11:03:40 +0100

    genml

--- classifier/cross_validation.py
+++ classifier/cross_validation.py
@@ -16,6 +16,7 @@ from sklearn.metrics import confusion_matrix, roc_auc_score
 from sklearn.metrics.classification import precision_recall_fscore_support
 from sklearn.metrics.scorer import make_scorer
 from sklearn.metrics import f1_score
+import numpy as np
 
 class CrossValidator(object):
     '''
