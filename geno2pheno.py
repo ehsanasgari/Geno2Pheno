281e7a049b4a8fc1783865c1deb6c24a518c4647
Ehsan Asgari <asgari@berkeley.edu>
2019-03-05 07:14:50 +0100

    new package testing

--- geno2pheno.py
+++ geno2pheno.py
@@ -185,13 +185,12 @@ class Geno2Pheno:
                     if not ultimate:
                         X, Y, feature_names, final_strains = GPA.get_xy_prediction_mats(feature_setting, phenotype, mapping)
 
-                        feature_setting =['_'.join(feature.split('.')[0:-1]) if len(feature.split('.'))>1 else feature for feature in feature_setting]
+                        feature_setting =[''.join(feature.split('.')[0:-1]) if len(feature.split('.'))>1 else feature for feature in feature_setting]
                         feature_text='##'.join(feature_setting)
 
                         ## iterate over classifiers
                         for classifier in tqdm.tqdm(classifiers):
                             basepath_cls=subdir+phenotype+'/'+feature_text+'_CV_'+self.cvbasis
-                            print(basepath_cls)
                             if classifier.lower()=='svm' and (not FileUtility.exists(basepath_cls+'_SVM.pickle') or self.override):
                                 Model = SVM(X, Y)
                                 Model.tune_and_eval_predefined(basepath_cls, final_strains, folds_file=cv_file, test_file=cv_test_file,njobs=self.cores, feature_names=feature_names)
